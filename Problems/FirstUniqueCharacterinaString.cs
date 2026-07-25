c08edc3a33ded8165946ad7035770e5365128259
Pavel-Sedlovec <pasha.sedlovetz@gmail.com>
2026-07-25 18:50:11 +0300

    add FirstUniqueCharacterinaString

---
+++ Problems/FirstUniqueCharacterinaString.cs
@@ -0,0 +1,36 @@
+﻿namespace Algorithms_CS.Problems
+{
+    // Given a string s, find the first non-repeating character in it
+    // and return its index. If it does not exist, return -1.
+    //
+    // Example 1:
+    // Input: s = "leetcode"
+    // Output: 0
+    // Explanation: The character 'l' at index 0 is the first character
+    // that does not occur at any other index.
+    //
+    // Example 2:
+    // Input: s = "loveleetcode"
+    // Output: 2
+    //
+    // Example 3:
+    // Input: s = "aabb"
+    // Output: -1
+    public class FirstUniqueCharacterinaString
+    {
+        public int FirstUniqChar(string s)
+        {
+            int[] arr = new int[256];
+            for (int i = 0; i < s.Length; i++)
+            {
+                arr[s[i]]++;
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (arr[s[i]] == 1)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
