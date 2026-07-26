05355d3cf17cce15d1d7497c3638d999fd60bd20
Pavel-Sedlovec <pasha.sedlovetz@gmail.com>
2026-07-26 19:48:18 +0300

    add MoveZeroes

---
+++ Problems/MoveZeroes.cs
@@ -0,0 +1,20 @@
+﻿namespace Algorithms_CS.Problems
+{
+    public class MoveZero
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int indexInsert = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[indexInsert];
+                    nums[indexInsert++] = temp;
+                }
+            }
+        }
+    }
+}
