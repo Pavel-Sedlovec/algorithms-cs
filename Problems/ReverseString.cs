1cecf5959bded8046e8674e27e45d7133687fc3a
Pavel-Sedlovec <pasha.sedlovetz@gmail.com>
2026-07-27 18:07:53 +0300

    add ReverseString

---
+++ Problems/ReverseString.cs
@@ -0,0 +1,30 @@
+﻿namespace Algorithms_CS.Problems
+{
+    // Write a function that reverses a string.
+    // The input string is given as an array of characters s.
+    // You must do this by modifying the input array in-place with O(1) extra memory.
+    //
+    // Example 1:
+    // Input: s = ["h","e","l","l","o"]
+    // Output: ["o","l","l","e","h"]
+    //
+    // Example 2:
+    // Input: s = ["H","a","n","n","a","h"]
+    // Output: ["h","a","n","n","a","H"]
+    public class ReverseStr
+    {
+        public void ReverseString(char[] s)
+        {
+            int l = 0;
+            int r = s.Length - 1;
+            while(r-l >= 0)
+            {
+                char temp = s[r];
+                s[r] = s[l];
+                s[l] = temp;
+                l++;
+                r--;
+            }
+        }
+    }
+}
