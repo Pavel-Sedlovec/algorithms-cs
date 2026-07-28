1cf7c28d23fffdc37daf3800c25fbab7e78f9eb2
Pavel-Sedlovec <pasha.sedlovetz@gmail.com>
2026-07-28 21:22:34 +0300

    add BestTimetoBuyandSellStock

---
+++ Problems/BestTimetoBuyandSellStock.cs
@@ -0,0 +1,21 @@
+﻿namespace Algorithms_CS.Problems
+{
+    public class BestTimetoBuyandSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int minBuy = prices[0];
+            int maxProfit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] - minBuy > maxProfit)
+                    maxProfit = prices[i] - minBuy;
+
+                if (prices[i] < minBuy)
+                    minBuy = prices[i];
+            }
+            return maxProfit;
+        }
+    }
+}
